26b0fe2ab3a0782a2014571b402ed2681cc225ec
mjefs <jefin@192.168.0.57>
2024-04-03 20:51:55 -0300

    if

--- primeiro_programa/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro_programa/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -4,8 +4,25 @@ public class PrimeiraClasseJava {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Helllo Word jefs");
-	}
-
+		int nota1 = 90;
+		int nota2 = 90;
+		int nota3 = 90;
+		int nota4 = 95;
+		int media = 0;
+		
+		media = (nota1 + nota2 + nota3 + nota4)/4;
+		
+		if(media >= 50) {
+			if(media >= 90) {
+				System.out.println("Parabéns você Foi aprovado acima de 90: " + media);
+			} else {
+				System.out.println("Parabéns você só foi aprovado com a média de: " + media);
+			}
+		} else {
+			System.out.println("Você foi reprovado com a média de: " + media);
+		}
+		
+		
+   }		
 }
 
